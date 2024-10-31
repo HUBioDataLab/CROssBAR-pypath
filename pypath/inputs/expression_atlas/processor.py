0067d316a8117fe76919e591848c9ae5389163fa
Havvanur Aydın <104494361+hnurydn@users.noreply.github.com>
2024-10-31 08:46:19 +0300

    Update processor.py

--- pypath/inputs/expression_atlas/processor.py
+++ pypath/inputs/expression_atlas/processor.py
@@ -97,7 +97,7 @@ class BaselineExperiementDataProcessor:
         # Drop rows if the "Sample Characteristic[matching_factor]" column contains " and "
         df = df[~df[f"Sample Characteristic[{self.matching_factor}]"].str.contains(" and ", na=False)]
 
-        df[f"Sample Characteristic[{self.matching_factor}]"] = df.apply(lambda row: row[f"Factor Value[{self.matching_factor}]"] if pd.notna(row[f"Factor Value[{self.matching_factor}]"]) and row[f"Sample Characteristic[{self.matching_factor}]"] != row[f"Factor Value[{self.matching_factor}]"] else row[f"Sample Characteristic[{self.matching_factor}]"], axis=1)
+        df[f"Sample Characteristic[{self.matching_factor}]"] = np.where((pd.notna(df[f"Factor Value[{self.matching_factor}]"])) & (pd.notna(df[f"Sample Characteristic[{self.matching_factor}]"])) & (df[f"Sample Characteristic[{self.matching_factor}]"] != df[f"Factor Value[{self.matching_factor}]"]), df[f"Factor Value[{self.matching_factor}]"], df[f"Sample Characteristic[{self.matching_factor}]"])
         
         if self.matching_factor == "cell line":
             df.loc[df[f"Sample Characteristic Ontology Term[{self.matching_factor}]"].isna(), f"Sample Characteristic Ontology Term[{self.matching_factor}]"] = df[f"Sample Characteristic[{self.matching_factor}]"]
